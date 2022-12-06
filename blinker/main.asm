f60a6bc8b53b6ee2c795f48a817244887fa7a200
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-12-06 10:35:59 +0530

    delay created

--- blinker/main.asm
+++ blinker/main.asm
@@ -11,11 +11,13 @@ start:
 SBI DDRB ,0 ;making pin 8 (port b pin 0) as an output pin
 SBI PORT ,0 ;making the pin 8 high
 
-
-
-	  LDI R18,255 ;load immediate 255 to register 18
+	  LDI R16,41
+LOOP1:LDI R17,255
+LOOP2:LDI R18,255 ;load immediate 255 to register 18
 LOOP3:DEC R18 ; decrement  by register 18
 	  BRNE LOOP3 ;branch to LOOP3 if R18 (Result) not equal to 0 
 	  DEC R17
 	  BRNE LOOP2
+	  DEC R16
+	  BRNE LOOP1
 
