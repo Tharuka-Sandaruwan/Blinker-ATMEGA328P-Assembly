d7b03bd787a36ea137832388b00ddb4eacf9bfbf
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-12-06 19:00:12 +0530

    c code complete

--- c code/blinker_C/blinker_C/main.c
+++ c code/blinker_C/blinker_C/main.c
@@ -6,13 +6,17 @@
  */ 
 
 #include <avr/io.h>
-
-
+#define F_CPU 16000000UL // defined the clock of the oscillator to be used for delay
+#include <util/delay.h> //library for delay
 int main(void)
 {
-    /* Replace with your application code */
+    DDRB = 0b00000001; //set bit 0 of portb as output
     while (1) 
     {
+		PORTB = 0b00000001; // set the port b register value to 00000001.it will turn on the pin 0 of port b
+		_delay_ms(200); //set delay 200 ms
+		PORTB = 0b00000000;
+		_delay_ms(200);
     }
 }
 
