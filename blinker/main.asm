9072fd217f1e32d4dd581dcfa60fad828558b24d
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-12-06 10:50:27 +0530

    delay functionalized

--- blinker/main.asm
+++ blinker/main.asm
@@ -1,17 +1,17 @@
-;
 ; blinker.asm
-;
 ; Created: 12/6/2022 9:58:17 AM
 ; Author : Tharuka Sandaruwan
-;
 
 
-; Replace with your application code
-SBI DDRB ,0 ;making pin 8 (port b pin 0) as an output pin
+	  SBI DDRB ,0 ;making pin 8 (port b pin 0) as an output pin
+START:SBI PORTB ,0 ;making the pin 8 high
+	  CALL DELAY ;delay function call	  
+	  CBI PORT ,0 ;clear the bit (lower pin 8)
+	  CALL DELAY
+	  JMP START ;goes again to start to run the loop infinitely
 
-START:SBI PORT ,0 ;making the pin 8 high
-
-	  LDI R16,41
+;BELOW IS THE DELAY FUNCTION
+DELAY:LDI R16,41
 LOOP1:LDI R17,255
 LOOP2:LDI R18,255 ;load immediate 255 to register 18
 LOOP3:DEC R18 ; decrement  by register 18
@@ -20,17 +20,4 @@ LOOP3:DEC R18 ; decrement  by register 18
 	  BRNE LOOP2
 	  DEC R16
 	  BRNE LOOP1
-
-CBI PORT ,0 ;clear the bit (lower pin 8)
-
-	  LDI R16,41
-LOOP1:LDI R17,255
-LOOP2:LDI R18,255
-LOOP3:DEC R18
-	  BRNE LOOP3
-	  DEC R17
-	  BRNE LOOP2
-	  DEC R16
-	  BRNE LOOP1
-
-JMP START ;goes again to start to run the loop infinitely
+	  RET
