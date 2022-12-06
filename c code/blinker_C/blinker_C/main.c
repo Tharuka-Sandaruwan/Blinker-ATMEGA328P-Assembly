ae1ebc3d19bb3f8aab313ada87a085f55540505c
Tharuka Sandaruwan <ts2000614@gmail.com>
2022-12-06 18:47:04 +0530

    c programme initialized

---
+++ c code/blinker_C/blinker_C/main.c
@@ -0,0 +1,18 @@
+/*
+ * blinker_C.c
+ *
+ * Created: 12/6/2022 6:46:04 PM
+ * Author : Tharuka Sandaruwan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
